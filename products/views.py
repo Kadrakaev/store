547198b329f596cf1754991c91b928b2589bbfa3
Kadrakaev <Kadrakaevv13@gmail.com>
2023-05-22 18:02:54 +0300

    fifth commit: added new model Basket added delete and add product from basket added protect for profile page, basket and add product

--- products/views.py
+++ products/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
-from products.models import Product, ProductCategory
+from products.models import Product, ProductCategory, Basket
+from users.models import User
+
+
 def index(request):
     return render(request, 'products/index.html')
+
 
 def products(request):
     context = {
@@ -11,3 +16,24 @@ def products(request):
         'categories': ProductCategory.objects.all()
     }
     return render(request, 'products/products.html', context)
+
+@login_required
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        baskets = baskets.first()
+        baskets.quantity += 1
+        baskets.save()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+@login_required
+def basket_remove(request, basket_id):
+    basket = Basket.objects.get(id=basket_id)
+    basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
-
+from products.models import Basket
 
 def login(request):
     if request.method == 'POST':
@@ -34,7 +35,7 @@ def register(request):
     context = {'form': form}
     return render(request, 'users/register.html', context)
 
-
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
@@ -43,8 +44,14 @@ def profile(request):
             return HttpResponseRedirect(reverse('users:profile'))
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Store - Профиль', 'form': form}
+
+    context = {
+        'title': 'Store - Профиль',
+        'form': form,
+        'baskets': Basket.objects.filter(user=request.user),
+    }
     return render(request, 'users/profile.html', context)
+
 
 def logout(request):
     auth.logout(request)
